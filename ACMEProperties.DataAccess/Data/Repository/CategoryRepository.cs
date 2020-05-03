0c78c8529458d15a2e98be8c287fa9b6a1e9fdd7
ThaymesCoder <thamue1892@gmail.com>
2020-05-03 23:59:17 +0200

    Implemented the IRepository and the Repository classes

---
+++ ACMEProperties.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACMEProperties.DataAccess.Data.Repository
+{
+    public class CategoryRepository
+    {
+    }
+}
---
+++ ACMEProperties.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACMEProperties.DataAccess.Data.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+    }
+}
