b66bf113eb6101e624ca40496fe6fe61ff195a0c
ThaymesCoder <thamue1892@gmail.com>
2020-05-03 14:42:48 +0200

    Created the Category Model

---
+++ ACMEProperties.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ACMEProperties.Models
+{
+    class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public int ShowOrder { get; set; }
+    }
+}
