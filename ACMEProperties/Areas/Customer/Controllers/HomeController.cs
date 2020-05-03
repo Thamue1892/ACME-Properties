1dc8d41e3fc7591eea5356644d0c436d7b626d71
ThaymesCoder <thamue1892@gmail.com>
2020-05-03 12:37:01 +0200

    Added areas to the project

--- ACMEProperties/Areas/Customer/Controllers/HomeController.cs
+++ ACMEProperties/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using ACMEProperties.Models;
 
 namespace ACMEProperties.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
