569653c340eb68fc164d1887ce4354b627b63d8c
ThaymesCoder <thamue1892@gmail.com>
2020-05-05 15:31:48 +0200

    Implemented the Category view and the Upsert view

---
+++ ACMEProperties/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ACMEProperties.DataAccess.Data.Repository.IRepository;
+using ACMEProperties.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACMEProperties.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id==null)
+            {
+                return View(category);
+            }
+
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category==null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id==0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        #region API CALLS
+
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Category.GetAll() });
+        }
+
+        #endregion
+    }
+
+
+}
