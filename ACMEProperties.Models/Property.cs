16f5681c398dd34f8cf718a2225b0050904692b4
ThaymesCoder <thamue1892@gmail.com>
2020-05-06 07:18:52 +0200

    Implemented the Property model

---
+++ ACMEProperties.Models/Property.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ACMEProperties.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Property Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Rooms")]
+        public int NumberOfRooms { get; set; }
+
+        [Required]
+        public int Bathrooms { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+    }
+}
--- ACMEProperties/Startup.cs
+++ ACMEProperties/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ACMEProperties.DataAccess;
+using ACMEProperties.DataAccess.Data.Repository;
+using ACMEProperties.DataAccess.Data.Repository.IRepository;
 
 namespace ACMEProperties
 {
@@ -24,10 +26,14 @@ namespace ACMEProperties
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddControllersWithViews().AddNewtonsoftJson().AddRazorRuntimeCompilation();
             services.AddRazorPages();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
