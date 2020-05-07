7d8904e5a378f5b5f3d48003653e8c87ef1cc8f2
ThaymesCoder <thamue1892@gmail.com>
2020-05-07 21:11:52 +0200

    Implemented Property Upsert method

--- ACMEProperties/Areas/Admin/Controllers/PropertyController.cs
+++ ACMEProperties/Areas/Admin/Controllers/PropertyController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ACMEProperties.DataAccess.Data.Repository.IRepository;
+using ACMEProperties.Models;
+using ACMEProperties.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ACMEProperties.Areas.Admin.Controllers
@@ -11,10 +15,15 @@ namespace ACMEProperties.Areas.Admin.Controllers
     public class PropertyController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public PropertyController(IUnitOfWork unitOfWork)
+        [BindProperty]
+        public PropertyViewModel PropertyViewModel { get; set; }
+
+        public PropertyController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
@@ -23,7 +32,85 @@ namespace ACMEProperties.Areas.Admin.Controllers
         }
 
 
-       
+        public IActionResult Upsert(int? id)
+        {
+            PropertyViewModel = new PropertyViewModel()
+            {
+                Property = new Models.Property(),
+                CategoryList = _unitOfWork.Category.GetCategoryListForDropDown(),
+                RentalList = _unitOfWork.Rental.GetRentalListForDropDown()
+            };
+            if (id != null)
+            {
+                PropertyViewModel.Property = _unitOfWork.Property.Get(id.GetValueOrDefault());
+            }
+
+            return View(PropertyViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                if (PropertyViewModel.Property.Id == 0)
+                {
+                    //New Property
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\property");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStreams);
+                    }
+
+                    PropertyViewModel.Property.ImageUrl = @"\images\Propertys\" + fileName + extension;
+
+                    _unitOfWork.Property.Add(PropertyViewModel.Property);
+                }
+                else
+                {
+                    //Edit Property
+                    var propertyFromDb = _unitOfWork.Property.Get(PropertyViewModel.Property.Id);
+                    if (files.Count > 0)
+                    {
+                        string fileName = Guid.NewGuid().ToString();
+                        var uploads = Path.Combine(webRootPath, @"images\property");
+                        var extension_new = Path.GetExtension(files[0].FileName);
+
+                        var imagePath = Path.Combine(webRootPath, propertyFromDb.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+
+                        using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension_new), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStreams);
+                        }
+
+                        PropertyViewModel.Property.ImageUrl = @"\images\property\" + fileName + extension_new;
+                    }
+                    else
+                    {
+                        PropertyViewModel.Property.ImageUrl = propertyFromDb.ImageUrl;
+                    }
+
+                    _unitOfWork.Property.Update(PropertyViewModel.Property);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(PropertyViewModel);
+            }
+        }
+
 
 
 
@@ -31,8 +118,14 @@ namespace ACMEProperties.Areas.Admin.Controllers
 
         public IActionResult GetAll()
         {
-            return Json(new {data = _unitOfWork.Property.GetAll()});
+            return Json(new { data = _unitOfWork.Property.GetAll(includeProperties: "Category,Rental") });
         }
+
+        //[HttpDelete]
+        //public IActionResult Delete(int id)
+        //{
+        //    var propertyFromDb = _unitOfWork.Property.Get(id);
+        //}
 
         #endregion
     }
