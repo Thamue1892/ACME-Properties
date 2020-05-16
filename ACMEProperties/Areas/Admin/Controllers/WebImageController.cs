8bbbae8c5c2b4f86d434e2fdf62c4eefb754bf85
ThaymesCoder <thamue1892@gmail.com>
2020-05-16 12:28:45 +0200

    Implemented WebImage Upsert

--- ACMEProperties/Areas/Admin/Controllers/WebImageController.cs
+++ ACMEProperties/Areas/Admin/Controllers/WebImageController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ACMEProperties.DataAccess;
+using ACMEProperties.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ACMEProperties.Areas.Admin.Controllers
@@ -20,6 +22,68 @@ namespace ACMEProperties.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            WebImages imageObj = new WebImages();
+            if (id==null)
+            {
+                
+            }
+            else
+            {
+                imageObj = _db.WebImages.SingleOrDefault(i => i.Id == id);
+                if (imageObj==null)
+                {
+                    return NotFound();
+                }
+            }
+
+            return View(imageObj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(int id, WebImages imageObj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count>0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1=files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+
+                    imageObj.Picture = p1;
+                }
+
+                if (imageObj.Id==0)
+                {
+                    _db.WebImages.Add(imageObj);
+                }
+                else
+                {
+                    var imageFromDb = _db.WebImages.FirstOrDefault(i => i.Id == id);
+
+                    imageFromDb.Name = imageFromDb.Name;
+                    if (files.Count>0)
+                    {
+                        imageFromDb.Picture = imageObj.Picture;
+                    }
+                }
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(imageObj);
         }
 
         #region API CALLS
