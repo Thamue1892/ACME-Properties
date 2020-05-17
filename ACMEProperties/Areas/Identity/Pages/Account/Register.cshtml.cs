2e262e9e4c9c167012363c92b869ba6985f36838
ThaymesCoder <thamue1892@gmail.com>
2020-05-17 21:39:48 +0200

    Registered the Post page handler

--- ACMEProperties/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ACMEProperties/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using ACMEProperties.Models;
+using ACMEProperties.Utility;
 
 namespace ACMEProperties.Areas.Identity.Pages.Account
 {
@@ -30,12 +31,14 @@ namespace ACMEProperties.Areas.Identity.Pages.Account
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         [BindProperty]
@@ -78,27 +81,36 @@ namespace ACMEProperties.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser
+                var user = new ApplicationUser()
                 {
                     UserName = Input.Email,
                     Email = Input.Email,
-                    //Name = Input.Name
+                    Name = Input.Name
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    if (!await _roleManager.RoleExistsAsync(SD.Admin))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Admin));
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Manager));
+                    }
+
+                    string role = Request.Form["rdUserRole"].ToString();
+
+                    if (role==SD.Admin)
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.Admin);
+                    }
+                    else
+                    {
+                        if (role==SD.Manager)
+                        {
+                            await _userManager.AddToRoleAsync(user, SD.Manager);
+                        } 
+                    }
+
                     _logger.LogInformation("User created a new account with password.");
-
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    //var callbackUrl = Url.Page(
-                    //    "/Account/ConfirmEmail",
-                    //    pageHandler: null,
-                    //    values: new { area = "Identity", userId = user.Id, code = code },
-                    //    protocol: Request.Scheme);
-
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
@@ -106,7 +118,6 @@ namespace ACMEProperties.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        //await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
                 }
