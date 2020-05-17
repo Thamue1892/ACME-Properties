4b9db7bb06c16ddcc815faabfb51c1acda5b5e46
ThaymesCoder <thamue1892@gmail.com>
2020-05-17 22:50:00 +0200

    Added User controller

---
+++ ACMEProperties/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ACMEProperties.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACMEProperties.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            return View(_unitOfWork.User.GetAll(u => u.Id != claims.Value));
+        }
+    }
+}
